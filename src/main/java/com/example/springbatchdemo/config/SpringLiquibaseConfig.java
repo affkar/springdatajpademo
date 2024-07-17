e524fbbf38879f58cbb19a4d271ad383931aa002
Karthick Meenakshi Sundaram <affkar@gmail.com>
2024-07-17 10:43:31 +0100

    Clean up config, pom.xml and update gitignore to ignore any idea related files.

--- src/main/java/com/example/springbatchdemo/config/SpringLiquibaseConfig.java
+++ src/main/java/com/example/springbatchdemo/config/SpringLiquibaseConfig.java
@@ -5,59 +5,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringLiquibaseConfig {
 
+    // This is not required, as it is provided by default. If any properties used by
+    // the default spring liquibase bean is to be overridden, then you might need
+    // this.
     // @Bean
-    // @ConfigurationProperties("spring.datasource.hikari")
-    // HikariConfig hikariConfig() {
-    // return new HikariConfig();
-    // }
+    // SpringLiquibase springLiquibase() {
 
-    // @Bean
-    // DataSource dataSource(final HikariConfig hikariConfig) {
-    // return new HikariDataSource(hikariConfig);
-    // }
-
-    // @Bean
-    // public Job cricketJob(JobRepository jobRepository, Step playerLoad, Step
-    // gameLoad, Step playerSummarization) {
-    // return new JobBuilder("cricketJob", jobRepository)
-    // // .validator(jobParametersValidator())
-    // .start(playerLoad)
-    // .next(gameLoad)
-    // .next(playerSummarization)
-    // .build();
-    // }
-
-    // private JobParametersValidator jobParametersValidator() {
-    // return new DefaultJobParametersValidator(new String[] { "team1Name",
-    // "team2Name", "gameFormat" },
-    // new String[0]);
-    // }
-
-    // @Bean
-    // public Step playerSummarization() {
-    // // TODO Auto-generated method stub
-    // throw new UnsupportedOperationException("Unimplemented method
-    // 'playerSummarization'");
-    // }
-
-    // @Bean
-    // public Step gameLoad() {
-    // // TODO Auto-generated method stub
-    // throw new UnsupportedOperationException("Unimplemented method 'gameLoad'");
-    // }
-
-    // @Bean
-    // public Step playerLoad(JobRepository jobRepository,
-    // PlatformTransactionManager platformTransactionManager,
-    // ItemReader<Player> jsonPlayerReader, ItemProcessor<Object, PlayerE>
-    // playerTransformer,
-    // ItemWriter<Object> dbPlayerWriter) {
-    // return new StepBuilder("playerLoad", jobRepository)
-    // .chunk(10, platformTransactionManager)
-    // .reader(jsonPlayerReader)
-    // .processor(playerTransformer)
-    // .writer(dbPlayerWriter)
-    // .build();
     // }
 
 }
