cfbec2e85e3a0cbf2c87d569b4cdc659141f547f
Karthick Meenakshi Sundaram <affkar@gmail.com>
2024-11-08 12:10:29 +0000

    Adding Mapstruct Mapper and removing the mapperImpl

--- src/main/java/com/example/springdatajpa/demo/mapper/PlayerMapper.java
+++ src/main/java/com/example/springdatajpa/demo/mapper/PlayerMapper.java
@@ -1,8 +1,11 @@
 package com.example.springdatajpa.demo.mapper;
 
+import org.mapstruct.Mapper;
+
 import com.example.springdatajpa.demo.entity.PlayerE;
 import com.example.springdatajpa.demo.model.Player;
 
+@Mapper(componentModel = "spring")
 public interface PlayerMapper {
 
     PlayerE mapToPlayerE(Player player);
