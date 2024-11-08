d14879e327a2b66cd78fcd580aeccd8e561d1e8f
Karthick Meenakshi Sundaram <affkar@gmail.com>
2024-11-08 17:07:36 +0000

    Add Global Exception Handler as another way of handling exceptions, in this case we are handling RuntimeException. Run MockMVC Tests as part of surefire, considering them as pretty much unit tests. Run DataJpa Tests as 1 execution and IT tests as 1 execution of failsafe plugin - reason being We want to reuse SpringContexts, as its creation is costly. If we had the DataJPATest and the ITs as part of a single execution, there is a good chance that SpringContexts will get recreated. it is better to rely on liquibase to generate the database schema as it would in Production and test the repository against that, but as part of DataJpaTests, only a part of the SpringContext is needed, so if there was a SpringContext already in place, it will destroy it and then try recreating a slice of the SPringContext, which is not ideal.

--- src/test/java/com/example/springdatajpa/demo/repository/PlayerRepositoryDataJpa.java
+++ src/test/java/com/example/springdatajpa/demo/repository/PlayerRepositoryDataJpa.java
@@ -11,16 +11,16 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import com.example.springdatajpa.demo.entity.PlayerE;
 
 @DataJpaTest
-public class PlayerRepositoryDataJpaIT {
+public class PlayerRepositoryDataJpa {
 
     @Autowired
     PlayerRepository testee;
 
     @Test
     void testSave() {
-        PlayerE savedPlayerEntity = testee.save(PlayerE.builder().name("Alagu").build());
+        final PlayerE savedPlayerEntity = this.testee.save(PlayerE.builder().name("Alagu").build());
         assertThat(savedPlayerEntity.getId()).isNotNull();
-        Optional<PlayerE> retrievedPlayerEntity = testee.findById(savedPlayerEntity.getId());
+        final Optional<PlayerE> retrievedPlayerEntity = this.testee.findById(savedPlayerEntity.getId());
         assertThat(retrievedPlayerEntity).isNotEmpty();
         assertThat(retrievedPlayerEntity.get())
                 .extracting(PlayerE::getId, PlayerE::getName)
