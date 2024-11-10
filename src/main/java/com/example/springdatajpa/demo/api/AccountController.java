cc66e108c7fdaa8cac77679e5061549106cc7ac6
Karthick Meenakshi Sundaram <affkar@gmail.com>
2024-11-10 01:21:42 +0000

    Add Account Controller Skeleton

---
+++ src/main/java/com/example/springdatajpa/demo/api/AccountController.java
@@ -0,0 +1,10 @@
+package com.example.springdatajpa.demo.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AccountController implements AccountApi {
+
+}
